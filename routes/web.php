bd664b145bc2fdfbcafa73a3a2652b3f8922c324
ReihanZakya <rezakya41@gmail.com>
2021-08-09 17:18:42 +0700

    tambah home controller

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('fitur.home');
-});
 
+Route::get('/','HomeController@index');
 Route::get('/RPL/fasilitas','FasilitasController@index');
 Route::get('/RPL/team','TeamController@index');
 Route::get('/RPL/kontak','KontakController@index');
