9ebab109afab8824deb3c60f9dd20a377040c36c
ReihanZakya <rezakya41@gmail.com>
2021-08-18 15:20:11 +0700

    kontak

--- app/Http/Controllers/KontakController.php
+++ app/Http/Controllers/KontakController.php
@@ -3,10 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class KontakController extends Controller
 {
     public function index(){
-        return view ('fitur.kontak');
+        return view ('fitur.contact');
+    }
+
+    public function kirim_pesan(Request $request){
+        $data = array(
+            'email' => $request->email,
+            'subject' => $request->subject,
+            'bodyMessage' => $request->message,
+        );
+        Mail::send('emails.contact', $data , function($message) use ($data){
+            $message->from($data['email']);
+            $message->to('rezakya41@gmail.com');
+            $message->subject($data['subject']);
+        });
+
+        return redirect()->back();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/','HomeController@index');
-Route::get('/RPL/fasilitas','FasilitasController@index');
 Route::get('/RPL/team','TeamController@index');
 Route::get('/RPL/kontak','KontakController@index');
+Route::post('/RPL/kontak','KontakController@kirim_pesan');
 Route::get('RPL/portofolio','PortofolioController@index');
 Route::get('RPL/peluang_kerja','PeluangKerjaController@index');
 Route::get('RPL/tujuan','TujuanController@index');
