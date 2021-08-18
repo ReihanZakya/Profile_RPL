b73f7588984d28c7d6462f0c090719a93ab4ed40
LuckmanMS <huxlexnormagomedov@gmail.com>
2021-08-18 09:24:32 +0700

    nambah footer

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -14,11 +14,10 @@
                 <strong>Email:</strong> smkmahaputra@gmail.com<br>
               </p>
               <div class="social-links mt-3">
-                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+
+                <a href="https://web.facebook.com/SMK-Mahaputra-Cerdas-Utama-1670924676571186" class="facebook"><i class="bx bxl-facebook"></i></a>
+                <a href="https://www.instagram.com/smkmahaputracerdasutama/" class="instagram"><i class="bx bxl-instagram"></i></a>
+                <a href="https://www.youtube.com/channel/UCCfYqV-2N44pFhsQpGEedCw/featured" class="linkedin"><i class="bx bxl-youtube"></i></a>
               </div>
             </div>
           </div>
@@ -26,20 +25,20 @@
           <div class="col-lg-4 col-md-6 footer-links">
             <h4>Tautan Bermanfaat</h4>
             <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Team</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Peluang Kerja</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Tujuan</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Portofolio</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="/">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="/RPL/team">Team</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="/RPL/peluang_kerja">Peluang Kerja</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="/RPL/tujuan">Tujuan</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="/RPL/portofolio">Portofolio</a></li>
             </ul>
           </div>
 
           <div class="col-lg-4 col-md-6 footer-newsletter">
-            <h4>Chanel Kita</h4>
-            <p>Jangan Lupa Untuk Mengunjungi Chanel Kita</p>
-            <form action="" method="post">
-              <input type="email" name="email"><input type="submit" value="Subscribe">
-            </form>
+            <h4>Kata-Kata Mutiara</h4>
+            <p></p>
+            <img src="{{asset('assets/img/quotess.jpg')}}" class="img-fluid quot" alt="">
+
+             
 
           </div>
 
