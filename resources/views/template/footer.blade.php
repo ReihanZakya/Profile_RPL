9a30855dcf00a3c1b734f8898d8697cdbc8112d4
LuckmanMS <huxlexnormagomedov@gmail.com>
2021-08-27 13:12:49 +0700

    [FOOTER] Menambah Testimoni

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -37,7 +37,7 @@
 
           <div class="col-lg-4 col-md-6 rating">
               <h4>Penilaian</h4>
-              <p class="nama">Testimoni Alumni</p>
+              <p class="nama">Apa kata Alumni</p>
               <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
           <div class="carousel-inner">
             <div class="carousel-item active">
@@ -51,7 +51,44 @@
                   bertemu zaman COVID-19. Terimakasih SMK MAHAPUTRA.Sukses selalu dan maju terus.
                   SMK BISA SMK HEBAT
               </p>
+              </div>
+              <div class="carousel-item">
+            <p>Malay Cahya Aprita</p>
+              <p class="tulisan">   Alhamdulillahnya banyak banget ilmu dan pelajaran yang diberikan, terutama ilmu tentang pembuatan web, dri hal yang mendasar,  diajarkan oleh para guru yang profesional dengan penuh kesabaran, 
+                dan tetap melatih dengan cara terbaiknya.
+              </p>
             </div>
+            <div class="carousel-item">
+            <p>Nadira NurulDwijaya</p>
+              <p class="tulisan">Kesan belajar RPL di SMK banyak, Bisa mendapatkan kesempatan untuk belajar ilmu pemrograman, bagaimana caranya problem solving, dan banyak lagi ilmu tentang dunia informatika yang bisa bermanfaat.
+              </p>
+              </div>
+              <div class="carousel-item">
+            <p>Aulia Nur Salam</p>
+              <p class="tulisan">belajar di SMK Mahaputra jurusan Rpl, jd bisa lebih tau tentang seluk-beluk perkodingan, bahkan pas main anonymous chat di tele sempet kepikiran ini kodingannya gimana meskipun yg main nya bot
+              </p>
+              </div>
+              <div class="carousel-item">
+            <p>Elsa Lusiani</p>
+              <p class="tulisan">Kesan belajar Rekayasa Perangkat Lunak bisa lebih mengenal dunia teknologi masa kini khususnya programming dimana metode belajar yang diterapkan lebih banyak praktik yang bisa membuat siswa banyak mengetahui masalah-masalah yang sering muncul dan mencari tau bagaimana cara mengatasinya.
+              </p>
+              </div>
+              <div class="carousel-item">
+            <p>Hania Mufianti Mufidah</p>
+              <p class="tulisan">belajar di mahaputra dengan jurusan RPL saya menjadi paham dengan bahasa pemrograman seperti PHP, C++, dan sebagainya.
+              Selain itu saya juga mengerti dengan hardware, terutama software
+              </p>
+              </div>
+              <div class="carousel-item">
+            <p>Yulianti Nur Zanah</p>
+              <p class="tulisan">Awalnya ngk suka pljran yg berhubungan dengan komputer,tpi karna masuk ke jurusan rpl ya udh pasti bljr tentang komputer,jdi di jalani aja,setiap pembelajaran pasti di perhatiin meski ngk suka tpi gmna atuh,kelamaan mungkin karna udah terbiasa karna kesehariannya main komputer jadi suka,tpi kalo mapelnya udah mulai susah nya kadang males,tpi ngk bisa mls2an,soalnya kan di target juga buat buat web nya,kadang malu kalo liat orang lain udh bisa tpi akunya masih blom bisa
+              </p>
+              </div>
+              <div class="carousel-item">
+              <p>Sumiartini Sri Rahayu</p>
+              <p class="tulisan">Dengan belajar di smk mahaputra terutama jurusan RPL menjadi bekal pada era digital 4.0 seperti saat ini, yang mana banyak menggunakan teknologi, kita jadi terbiasa dengan perkembangan teknologi yg ada kedepannya.
+              </p>
+              </div>
           </div>
         </div>
 
