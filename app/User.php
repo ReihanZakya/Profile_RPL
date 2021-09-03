e24d25bf3186f95d402c42cd716bcfe57011d373
ReihanZakya <rezakya41@gmail.com>
2021-09-03 15:54:03 +0700

    [auth] delete level

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','level',
+        'name', 'email', 'password',
     ];
 
     /**
