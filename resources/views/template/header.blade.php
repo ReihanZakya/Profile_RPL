a7efbb7ced688185a7af93cd9ced8352f8270f79
LuckmanMS <huxlexnormagomedov@gmail.com>
2021-09-08 16:09:10 +0700

    mencoba membenarkan bug

--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -6,6 +6,7 @@
         <!-- Uncomment below if you prefer to use an image logo -->
         <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
+       
         <nav id="navbar" class="navbar order-last order-lg-0 ">
             <ul class="nav ml-auto">
                 <li class="nav-item"><a class="link {{ url()->current() == url('/') ? 'active' : '' }}" href="/">Beranda</a></li>
@@ -20,20 +21,6 @@
                         href="{{ url('RPL/portfolio') }}">Portofolio</a></li>
                 <li class="nav-item"><a href="{{ url('RPL/contact') }}"
                         class="link {{ url()->current() == url('RPL/contact') ? 'active' : '' }}">Kontak</a></li>
-        <nav id="navbar" class="navbar order-last order-lg-0">
-            <ul>
-                <li><a class="link {{ Request::path() === '/' ? 'active' : '' }}" href="/">Beranda</a></li>
-                <li><a class="link {{ Request::path() === 'RPL/mata_pelajaran' ? 'active' : '' }}"
-                        href="{{ url('RPL/mata_pelajaran') }}">Mata Pelajaran</a></li>
-                <li><a class="link {{ Request::path() === 'RPL/guru' ? 'active' : '' }}"
-                        href="{{ url('RPL/guru') }}">Guru</a></li>
-                <li><a class="link {{ Request::path() === 'RPL/peluang_kerja' ? 'active' : '' }}"
-                        href="{{ url('RPL/peluang_kerja') }}">Peluang Kerja</a></li>
-                {{-- <li><a class="link " href="/RPL/tujuan">Tujuan</a></li> --}}
-                <li><a class="link {{ Request::path() === 'RPL/portfolio' ? 'active' : '' }}"
-                        href="{{ url('RPL/portfolio') }}">Portofolio</a></li>
-                <li><a href="{{ url('RPL/contact') }}"
-                        class="link {{ Request::path() === 'RPL/contact' ? 'active' : '' }}">Kontak</a></li>
                 </li>
             </ul>
             <i class="bi bi-list mobile-nav-toggle"></i>
