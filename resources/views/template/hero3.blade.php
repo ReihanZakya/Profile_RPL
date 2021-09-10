91c02b4ce33018fc0f35d21bf165cc2d818acce2
LuckmanMS <huxlexnormagomedov@gmail.com>
2021-09-10 14:44:03 +0700

    REVISI WEB

---
+++ resources/views/template/hero3.blade.php
@@ -0,0 +1,44 @@
+<section id="hero3" class="d-flex align-items-center justify-content-center ">
+    <div class="container" data-aos="fade-up">
+
+      <div class="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
+        <div class="col-xl-6 col-lg-8">
+          <h1>Rekayasa Perangkat Lunak<span></span></h1>
+        </div>
+      </div>
+
+      {{-- <div class="row gy-4 mt-5 justify-content-center" data-aos="zoom-in" data-aos-delay="250">
+        <div class="col-xl-2 col-md-4">
+          <div class="icon-box">
+            <i class="ri-store-line"></i>
+            <h3><a href="team/">Team</a></h3>
+          </div>
+        </div>
+        <div class="col-xl-2 col-md-4">
+          <div class="icon-box">        
+            <i class="ri-bar-chart-box-line"></i>
+            <h3><a href="">Dolor Sitema</a></h3>
+          </div>
+        </div>
+        <div class="col-xl-2 col-md-4">
+          <div class="icon-box">
+            <i class="ri-calendar-todo-line"></i>
+            <h3><a href="fasilitas/">Fasilitas</a></h3>
+          </div>
+        </div>
+        <div class="col-xl-2 col-md-4">
+          <div class="icon-box">
+            <i class="ri-paint-brush-line"></i>
+            <h3><a href="">Magni Dolores</a></h3>
+          </div>
+        </div>
+        <div class="col-xl-2 col-md-4">
+          <div class="icon-box">
+            <i class="ri-database-2-line"></i>
+            <h3><a href="kontak/">Kontak</a></h3>
+          </div>
+        </div>
+      </div> --}}
+
+    </div>
+  </section>
