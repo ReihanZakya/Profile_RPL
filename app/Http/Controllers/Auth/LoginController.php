4d8a2eb8cdbc69951e18a1c51dcc3c296819c099
Dedenalif <denlif34690@gmail.com>
2021-09-14 10:28:04 +0700

    Revert "[Backend]  Fitur-fitur"
    This reverts commit 92ccb86ce731d6ce60e11d5e91c78f13e3499670.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -37,14 +36,5 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
-    }
-
-    protected function authenticated(Request $request, $user)
-    {
-        if($user->hasRole('admin')){
-            return redirect('dashboard');
-        }
-
-        return redirect('dashboard');
     }
 }
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,7 +8,6 @@ use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
@@ -54,6 +53,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:3', 'confirmed'],
+            'level' => ['required', 'string', 'max:255'],
         ]);
     }
 
@@ -65,19 +65,11 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        $user = User::create([
+        return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'level' => $data['level'],
         ]);
-
-        $user->assignRole('admin');
-
-        return $user;
-    }
-
-    protected function authenticated(Request $request, $user)
-    {
-        return redirect('dashboard');
     }
 }
