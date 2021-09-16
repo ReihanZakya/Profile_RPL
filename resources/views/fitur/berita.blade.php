13a17903370fbcc24fe2cc003f0ea1250b50dcb7
LuckmanMS <huxlexnormagomedov@gmail.com>
2021-09-16 09:09:58 +0700

    [BERITA] Revisi

--- resources/views/fitur/berita.blade.php
+++ resources/views/fitur/berita.blade.php
@@ -2,7 +2,31 @@
 
 @section('content')
 
+  
+<div id="carouselExampleControls" class="carousel slide " data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="{{ asset('assets/img/sunburst.png') }}" class="d-block w-100 cover" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="{{ asset('assets/img/hacker4.png') }}" class="d-block w-100 cover" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="{{ asset('assets/img/hacker3.png') }}" class="d-block w-100 cover" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+                                     
 <div class="container" data-aos="fade-up">
+
                 <div class="section-title">
                     <h2 style="color: orange;">Berita Global</h2>
                     <p>Seputar Rekayasa Perangkat Lunak</p>
