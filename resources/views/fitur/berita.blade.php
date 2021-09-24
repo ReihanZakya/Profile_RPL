b4cc8251c0a6c5379f8d97cd92307f475d0a4eca
ReihanZakya <rezakya41@gmail.com>
2021-09-24 09:30:20 +0700

    [revisi] kontak, berita portfolio

--- resources/views/fitur/berita.blade.php
+++ resources/views/fitur/berita.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-  
+
 <div id="carouselExampleControls" class="carousel slide " data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
@@ -28,7 +28,7 @@
     <span class="visually-hidden">Next</span>
   </button>
 </div>
-                                     
+
 <div class="container" data-aos="fade-up">
 
                 <div class="section-title">
@@ -41,34 +41,34 @@
               <div class="row g-3">
 
                     <div class="col-md-4 col-sm-12 mb-3">
-                            <div class="card shadow-sm garis">
+                        <div class="card shadow-sm garis">
                                 <img src="{{ asset('assets/img/sunburst.png') }}" class="card-img"  alt=""
                                     class="bd-placeholder-img card-img-top" width="100%" height="232"
                                     xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail"
                                     preserveAspectRatio="xMidYMid slice" focusable="false">
 
-                                <div class="card-body" >
+                            <div class="card-body" >
                                     <h5 class="card-text  warna">Apa Itu "Sunburst"?</h5>
                                     <p class="card-text">
                                     </p>
                                     <!-- <button type="button" class="btn btn-primary">SELENGKAPNYA -->
-                                    <button type="button" class="btn btn-primary"><a href="{{ url('RPL/berita1') }}"class="link {{ url()->current() == url('RPL/berita1') ? 'active' : '' }}" style="font-size: 14px; color: white;">SELENGKAPNYA</button>
+                                    <button type="button" class="btn btn-dark"><a href="{{ url('RPL/berita1') }}"class="link {{ url()->current() == url('RPL/berita1') ? 'active' : '' }}" style="font-size: 14px; color: orange;">SELENGKAPNYA</button>
                                     <!-- </button> -->
                                     <a></a>
                                                       </div>
                                     <div class="modal fade" id="webdeveloper" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                                  <div class="modal-dialog">
-                                                    <div class="modal-content">
-                                                      <div class="modal-header">
-                                                     
-                                                  </div>
-                                                </div>
-                                                </div> 
-                                                                                               
+                                            <div class="modal-dialog">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+
+                                            </div>
+                                        </div>
+                                    </div>
+
                                 </div>
                             </div>
-                          </div>
-                          
+                        </div>
+
                             <div class="col-md-4 col-sm-12 mb-3">
                             <div class="card shadow-sm garis">
                                 <img src="{{ asset('assets/img/hacker4.png') }}" alt=""
@@ -79,15 +79,15 @@
                                 <div class="card-body" >
                                     <h5 class="card-text warna">Bulan-bilanan RI di Tangan Peretas.</h5>
                                     <p class="card-text">
-                                    
+
                                     </p>
-                                    <button type="button" class="btn btn-primary"><a href="{{ url('RPL/berita2') }}"class="link {{ url()->current() == url('RPL/berita2') ? 'active' : '' }}" style="font-size: 14px; color: white;">SELENGKAPNYA</button>
+                                    <button type="button" class="btn btn-dark"><a href="{{ url('RPL/berita2') }}"class="link {{ url()->current() == url('RPL/berita2') ? 'active' : '' }}" style="font-size: 14px; color: orange;">SELENGKAPNYA</button>
 
                                     <a></a>
 
-                                    
-                                                
-                                    
+
+
+
 
                                     <div class="modal fade" id="webdesign" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                                   <div class="modal-dialog">
@@ -97,20 +97,20 @@
                                                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                       </div>
                                                       <div class="modal-body">
-                                                        
-                                                     
+
+
                                                       </div>
-                                                     
+
                                                     </div>
                                                   </div>
                                                 </div>
                                     <div class="d-flex justify-content-between align-items-center">
-                                       
+
                                     </div>
                                 </div>
                             </div>
                         </div>
-                                                 
+
                         <div class="col-md-4 col-sm-12 mb-3">
                             <div class="card shadow-sm garis">
                                 <img src="{{ asset('assets/img/hacker3.png') }}" alt=""
@@ -121,13 +121,13 @@
                                 <div class="card-body">
                                     <h5 class="card-text warna">Hacker China Membobol BIN. </h5>
                                     <p class="card-text">
-                                   
+
                                     </p>
-                                    <button type="button" class="btn btn-primary"><a href="{{ url('RPL/berita3') }}"class="link {{ url()->current() == url('RPL/berita3') ? 'active' : '' }}" style="font-size: 14px; color: white;">SELENGKAPNYA</button>
+                                    <button type="button" class="btn btn-dark"><a href="{{ url('RPL/berita3') }}"class="link {{ url()->current() == url('RPL/berita3') ? 'active' : '' }}" style="font-size: 14px; color: orange;">SELENGKAPNYA</button>
 
                                     <a></a>
 
-              
+
 
                                     <div class="modal fade" id="hacker" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                                   <div class="modal-dialog">
@@ -137,13 +137,13 @@
                                                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                       </div>
                                                       <div class="modal-body">
-                                                        
-                                                     
+
+
                                                     </div>
                                                   </div>
                                                 </div>
                                     <div class="d-flex justify-content-between align-items-center">
-                                        
+
                                     </div>
                                 </div>
                             </div>
@@ -153,5 +153,5 @@
                     </div>
                     </div>
                     </div>
-             
+
 @endsection
