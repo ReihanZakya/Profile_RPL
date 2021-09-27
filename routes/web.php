f2e7d98ea96de3bafd766f34a9a837b8afc270d3
ReihanZakya <rezakya41@gmail.com>
2021-09-27 11:42:19 +0700

    [clear] menghapus yang tidak perlu

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,6 @@ Route::get('/RPL/mata_pelajaran','Mata_PelajaranController@index');
 Route::get('/RPL/guru','GuruController@index');
 
 Route::get('/RPL/contact','ContactController@index');
-Route::post('/RPL/contact','ContactController@kirim_pesan');
 
 Route::get('RPL/portfolio','PortfolioController@index');
 
@@ -25,7 +24,7 @@ Route::get('RPL/berita1','Berita1Controller@index');
 Route::get('RPL/berita2','Berita2Controller@index');
 
 Route::get('RPL/berita3','Berita3Controller@index');
-    
+
 
 
 
