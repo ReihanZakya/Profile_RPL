f957f8279acce7fb9cbf074070b435fa2398d913
LuckmanMS <huxlexnormagomedov@gmail.com>
2021-09-30 10:04:51 +0700

    [BERITA] Revisi

--- resources/views/fitur/berita.blade.php
+++ resources/views/fitur/berita.blade.php
@@ -32,11 +32,11 @@
   </div>
   </div> 
   
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+  <button class="carousel-control-prev prev-button" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden ">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+  <button class="carousel-control-next next-button" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden-="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
